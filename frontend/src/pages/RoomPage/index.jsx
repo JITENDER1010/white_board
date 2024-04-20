ecd60e625c95659eb784b7e0f14b1e6118cb3e81
JITENDER1010 <100793986+JITENDER1010@users.noreply.github.com>
2024-04-20 07:46:52 +0530

    undo redo
    undo redo button working properly

--- frontend/src/pages/RoomPage/index.jsx
+++ frontend/src/pages/RoomPage/index.jsx
@@ -10,6 +10,7 @@ const RoomPage = () => {
     const [tool, setTool] = useState("pencil");
     const[color , setColor] = useState("black");
     const [elements , setElements] = useState([]);
+    const [history , setHistory] = useState([]);
      const handleClearCanvas = () => {
         const canvas = canvasRef.current;
         const ctx = canvas.getContext("2d");
@@ -17,6 +18,20 @@ const RoomPage = () => {
         ctxRef.current.clearRect(0 , 0 , canvasRef.current.width , canvasRef.current.height);
         setElements([]);
      }
+     const undo = () => {
+     setHistory((prevHistory) => [...prevHistory , elements[elements.length - 1]]);
+     setElements(
+        (prevElements) => prevElements.slice(0 , prevElements.length -1)
+        
+     )
+     }
+     const redo = () => {
+        setElements((prevElements) => [...prevElements , history[history.length - 1]]);
+        setHistory(
+           (prevHistory) => prevHistory.slice(0 , prevHistory.length -1)
+           
+        )
+        }
     return (
         <div className="row">
             <h1 className="text-center pt-4  py-4">White board sharing app <span className="text-primary"> [User Online-0]</span></h1>
@@ -71,8 +86,16 @@ const RoomPage = () => {
                 </div>
                 </div>
                 <div className="col-md-3 d-flex gap-2">
-                    <button className="btn btn-primary mt-1">Undo</button>
-                    <button className="btn btn-outline-primary mt-1">Redo</button>
+                    <button className="btn btn-primary mt-1" 
+                    disabled = {elements.length === 0}
+                    onClick={() => undo()}
+
+
+                    >Undo</button>
+                    <button className="btn btn-outline-primary mt-1"
+                     disabled = {history.length < 1}
+                     onClick={() => redo()}
+                     >Redo</button>
                 </div>
                 <div className="col-md-3">
                     <button className="btn btn-danger" onClick = {handleClearCanvas}>Clear Canvas</button>
