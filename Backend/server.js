24fa9c6db3ec609ed7ddbeee7ec5318b7a0be92c
JITENDER1010 <100793986+JITENDER1010@users.noreply.github.com>
2024-04-25 00:11:38 +0530

    white board
    socket connect for white board

--- Backend/server.js
+++ Backend/server.js
@@ -8,12 +8,23 @@ const {Server} = require("socket.io");
 app.get("/" , (req , res) => {
     res.send("this is mern realtime board sharing app offical server ");
 });
-
+let roomIdGlobal , imgURLGlobal;
 io.on("connection" , (socket) => {
     socket.on("userJoined" , (data) =>{
         const {name , userId , roomId , host , presenter} = data;
+        roomIdGlobal = roomId;
         socket.join(roomId);
-        socket.emit("userIsJoined" , {success:true})
+        socket.emit("userIsJoined" , {success:true});
+        socket.broadcast.to(roomId).emit("whiteboardDataResponse" , {
+            imgURL: imgURLGlobal,
+        })
+    });
+    socket.on("whiteboardData" , (data)=>{
+        imgURLGlobal = data;
+        socket.broadcast.to(roomIdGlobal).emit("whiteboardDataResponse",{
+            imgURL:data,
+      })
+
     })
 })
 const port = process.env.PORT || 5000;
